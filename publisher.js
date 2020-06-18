0360bd9bcd411cf78ba09da254f04d204bd4f3e7
zubair001 <zubairtub@gmail.com>
2020-06-18 03:55:16 +0200

    Initial commit

---
+++ publisher.js
@@ -0,0 +1,16 @@
+const amqp = require("amqplib");
+
+const msg={number: process.argv[2]}
+connect();
+async function connect(){
+    try{
+        const connection = await amqp.connect("amqp://localhost:5672");
+        const channel = await connection.createChannel();
+        const result= await channel.assertQueue("jobs");
+        channel.sendToQueue("jobs", Buffer.from(JSON.stringify(msg)));
+        console.log(`Jobs sent successfully ${msg.number}`);
+
+    }
+    catch (ex) {
+        console.log(ex);
+    }}
